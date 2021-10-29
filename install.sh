fdf180617aa3c41dfc228ce5f0f3144febe90dbb
AwesomeWaffle <35811077+AwesomeWaffle@users.noreply.github.com>
2021-10-29 11:29:51 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,11 +7,13 @@ echo Updating APT
 sudo apt-get update
 echo Installing Python3 dev
 sudo apt-get install python3-pip python-dev
-echo Installing UNICORN pHAT library
-sudo pip3 install unicornhat
+echo Installing UNICORN HD pHAT library
+sudo curl https://get.pimoroni.com/unicornhathd | bash
 echo Installing qrcode module
 sudo pip3 install pyqrcode
 echo Installing gpiozero 
 sudo pip3 install gpiozero
+echo Installing Rpi.GPIO
+sudo pip3 install Rpi.GPIO
 echo Installing Microsoft Office authentication library
 sudo pip3 install msal
